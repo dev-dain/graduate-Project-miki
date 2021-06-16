253627ac81f9f229e976d4aba2987bff8368fd77
Dain Kim <dev.dain.k@gmail.com>
2021-06-17 01:08:47 +0900

    장바구니에서 수량 변경 시 fetch 보내 즉시 DB에 반영하도록 변경

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,12 +1,7 @@
-const goPrevBtn = document.querySelector('.go-prev');
 const selectAllBtn = document.querySelector('.select-all-btn');
 let isSelectAll = true;
 
-goPrevBtn.addEventListener('click', () => { history.back(); });
 selectAllBtn.addEventListener('click', () => {
   isSelectAll = !isSelectAll;
   selectAllBtn.innerHTML = (isSelectAll) ? '&#10004;' : '';
-  //////////////// 상품들 전체 select/unselect 로직 필요
-  //
-  ////////////////
 });
