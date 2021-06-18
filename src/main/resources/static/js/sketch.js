49d06cb2fba51af8f5b2e6d4415ded76f9ceb4cb
daeunblue <vkfksqlcgkxm@naver.com>
2021-06-18 16:51:27 +0900

    test

--- src/main/resources/static/js/sketch.js
+++ src/main/resources/static/js/sketch.js
@@ -28,48 +28,51 @@ function modelReady() {
 
 function draw() {
   image(video, 0, 0, width, height);
-  drawKeypoints(R,G,B,A,pos);
+  drawKeypoints();
 }
 
 
 
-function drawKeypoints(R,G,B,A,pos){
+function drawKeypoints() {
+  //function drawKeypoints(R,G,B,A,pos){
   for (let i = 0; i < predictions.length; i += 1) {
     sil = predictions[i].annotations;
     noStroke();
-    
+
+    fill(255, 0, 0, 50);
+    face.lips();
     // fill(255, 255, 255, 50); //얼굴 윤곽 rgb+투명도
     // face.silhouette(); 
-    switch (pos){
-    case L:
-      fill(R,G,B,A);
-      face.lips();
-    case C:
-        /* fill cheek position */
-    case B:
-      /* fill eyebrow position */
-    default :
-      return 0;
+    // switch (pos){
+    // case 'L':
+    //   fill(R,G,B,A);
+    //   face.lips();
+    // case 'C':
+    //     /* fill cheek position */
+    // case 'B':
+    //   /* fill eyebrow position */
+    // default :
+    //   return 0;
   }
-    
+
 //     fill(255,0,0, 50); //입술 rgb+투명도
 //     fill(R,G,B,A);
 //     face.lips();
-    
+
 //     fill(0,0,255, 50); //오른쪽 눈 꺼풀 rgb+투명도
 //     face.rightEyeUpper();
-    
+
 //     fill(0,0,0, 50); //오른쪽 눈 아래 rgb+투명도
 //     face.rightEyeLower();
-    
+
 //     fill(0,0,255, 50); //왼쪽 눈 꺼풀 rgb+투명도
 //     face.leftEyeUpper();
-    
+
 //     fill(0,0,0, 50); //왼쪽 눈 아래 rgb+투명도
 //     face.leftEyeLower();
-    
-    //볼 좌우 연지곤지(수정필요)
-    // ellipse(sil.rightCheek[0][0], sil.rightCheek[0][1], 20 , 20); 
-    // ellipse(sil.leftCheek[0][0], sil.leftCheek[0][1], 20 , 20);
-  }
+
+  //볼 좌우 연지곤지(수정필요)
+  // ellipse(sil.rightCheek[0][0], sil.rightCheek[0][1], 20 , 20);
+  // ellipse(sil.leftCheek[0][0], sil.leftCheek[0][1], 20 , 20);
+
 }
