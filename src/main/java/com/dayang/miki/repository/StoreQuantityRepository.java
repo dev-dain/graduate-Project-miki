6e176ef770e01c78fffdd3fbd65a259ef4058ae1
yangyunhui454545 <codingbutter@naver.com>
2021-07-18 23:08:07 +0900

    parameter 수정

--- src/main/java/com/dayang/miki/repository/StoreQuantityRepository.java
+++ src/main/java/com/dayang/miki/repository/StoreQuantityRepository.java
@@ -4,6 +4,7 @@ import com.dayang.miki.domain.Item_option;
 import com.dayang.miki.domain.StoreQuantity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface StoreQuantityRepository extends JpaRepository<StoreQuantity, Long> {
 
     @Query("select sq from StoreQuantity sq where sq.item_option =:item_option")
-    List<StoreQuantity> findByItemOptions(@RequestParam("item_option") Item_option item_option);
+    List<StoreQuantity> findByItemOptions(@Param("item_option") Item_option item_option);
 }
