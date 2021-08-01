151fedc922cbe9946222ac917f6a5159f815cbc1
daeunblue <vkfksqlcgkxm@naver.com>
2021-08-02 03:07:20 +0900

    cart - 한꺼번에 다 가져오고 + 각 부위별 첫 제품만 적용 완료

--- src/main/resources/static/js/sketch.js
+++ src/main/resources/static/js/sketch.js
@@ -32,33 +32,75 @@ function draw() {
   drawKeypoints();
 }
 
+let Lip = false;
+let cheek = false;
+let brow = false;
+
 function drawKeypoints() {
   // background(0, 0, 0, 150);
   for (let i = 0; i < predictions.length; i += 1) {
     sil = predictions[i].annotations;
     noStroke();
- 
-     switch (position) {
-      case 'L':
+
+    if(is_L == true){
         /* fill lip position */
-        fill(R,G,B,alpha*100); // alpha 값이 0.x로 넘어오기때문에 * 100 필요
+        fill(L_R,L_G,L_B,alpha*100); // alpha 값이 0.x로 넘어오기때문에 * 100 필요
         face.lips();
-        break;
-      case 'C':
-        fill(R, G, B, alpha*10); //투명도 10
+        if (is_C == true){
+            fill(C_R, C_G, C_B, alpha*20); //투명도 10
+            face.leftCheeck();  //볼터치
+            face.rightCheeck();
+            if (is_B == true){
+                fill(B_R, B_G, B_B, alpha*100);
+                face.leftEyebrow();  //눈썹
+                face.rightEyebrow();
+            }
+        }
+        else if (is_B == true){
+            fill(B_R, B_G, B_B, alpha*100);
+            face.leftEyebrow();  //눈썹
+            face.rightEyebrow();
+        }
+
+    }
+    else if (is_C == true){
+        fill(C_R, C_G, C_B, alpha*20); //투명도 10
         face.leftCheeck();  //볼터치
         face.rightCheeck();
-          break;
-       case 'B':
-        console.log("브로우 진입");
-        /* fill eyebrow position */
-        fill(R, G, B, alpha*100);
+        if (is_B == true){
+            fill(B_R, B_G, B_B, alpha*100);
+            face.leftEyebrow();  //눈썹
+            face.rightEyebrow();
+        }
+    }
+    else if (is_B == true){
+        fill(B_R, B_G, B_B, alpha*100);
         face.leftEyebrow();  //눈썹
         face.rightEyebrow();
-           break;
-      default :
-        return 0;
     }
+    // Lip Cheek Brow 가 이미 적용되어있는지 사전 확인이 필요함
+
+    //  switch (position) {
+    //   case 'L':
+    //     /* fill lip position */
+    //     fill(R,G,B,alpha*100); // alpha 값이 0.x로 넘어오기때문에 * 100 필요
+    //     face.lips();
+    //     break;
+    //   case 'C':
+    //     fill(R, G, B, alpha*100); //투명도 10
+    //     face.leftCheeck();  //볼터치
+    //     face.rightCheeck();
+    //       break;
+    //    case 'B':
+    //     console.log("브로우 진입");
+    //     /* fill eyebrow position */
+    //     fill(R, G, B, alpha*100);
+    //     face.leftEyebrow();  //눈썹
+    //     face.rightEyebrow();
+    //        break;
+    //   default :
+    //     return 0;
+    // }
 
   }
 }
