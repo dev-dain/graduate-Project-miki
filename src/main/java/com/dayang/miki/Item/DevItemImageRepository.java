3845ff29c50fc72dc6455fe22347be50ac9c9cef
yangyunhui454545 <codingbutter@naver.com>
2021-08-26 17:11:11 +0900

    ITEM API 수정 중

---
+++ src/main/java/com/dayang/miki/Item/DevItemImageRepository.java
@@ -0,0 +1,10 @@
+package com.dayang.miki.Item;
+
+import com.dayang.miki.domain.Item;
+import com.dayang.miki.domain.Item_img;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DevItemImageRepository extends JpaRepository<Item_img, Long> {
+
+    Item_img findDistinctByItem (Item item);
+}
